27a1028edf5eff62cbda74b04390a09c5c989961
iFelipeBento <36781161+iFelipeBento@users.noreply.github.com>
2018-08-27 10:52:58 -0300

    Add files via upload

---
+++ While.java
@@ -0,0 +1,16 @@
+package Introducao.ControleDeFluxo;
+
+public class While {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		while(i<10) {
+			System.out.println(i);
+			
+			i++; //Obrigatório o incremento, caso contrário loop infinito.
+		}
+		
+	}
+
+}
