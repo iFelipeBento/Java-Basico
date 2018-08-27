2aa98f26de0b04915febfb2fb963f91f4a2ecc5c
iFelipeBento <36781161+iFelipeBento@users.noreply.github.com>
2018-08-27 10:55:50 -0300

    Add files via upload

---
+++ For.java
@@ -0,0 +1,16 @@
+package Introducao.ControleDeFluxo;
+
+public class For {
+	
+	public static void main(String[] args) {
+		
+		for(int i=10; i>0; i--) { 		
+			
+			//Criei e inicializei a variavél i
+			
+			System.out.println(i);
+		}
+		
+	}
+
+}
